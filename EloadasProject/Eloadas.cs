6fbb9b8aa219d2cf66218b75479b35d3ad1468b0
Zalán Bölkény <Kismajesz@gmail.com>
2020-03-06 09:14:39 +0100

    asd

---
+++ EloadasProject/Eloadas.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        private const string SOR_HIBA = "A soroknak nagyobbnak kell lennie nullánál";
+        private const string HELY_HIBA = "A helyeknek nagyobbnak kell lennie nullánál";
+        bool[,] rendelesek;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (sorokSzama < 1)
+                throw new ArgumentException(String.Format(SOR_HIBA));
+            if (helyekSzama < 1)
+                throw new ArgumentException(String.Format(HELY_HIBA));
+            else
+            {
+                rendelesek = new bool[sorokSzama, helyekSzama];
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        rendelesek[i, j] = false;
+                    }
+                }
+            }
+        }
+
+        public bool Lefoglal()
+        {
+            for (int i = 0; i < rendelesek.GetLength(0); i++)
+            {
+                for (int j = 0; j < rendelesek.GetLength(1); j++)
+                {
+                    if (!rendelesek[i, j])
+                    {
+                        rendelesek[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public int HelySzabad()
+        {
+            int s = 0;
+            for (int i = 0; i < rendelesek.GetLength(0); i++)
+            {
+                for (int j = 0; j < rendelesek.GetLength(1); j++)
+                {
+                    if (!rendelesek[i, j])
+                    {
+                        s++;
+                    }
+                }
+            }
+            return s;
+        }
+
+        public bool Full()
+        {
+            return HelySzabad() == 0 ? true : false;
+        }
+
+        public bool Lefoglalva(int sorSzam, int helySzam)
+        {
+            if (sorSzam < 1)
+
+                throw new ArgumentException(String.Format(SOR_HIBA));
+            if (helySzam < 1)
+
+                throw new ArgumentException(String.Format(HELY_HIBA));
+
+            return rendelesek[sorSzam - 1, helySzam - 1];
+        }
+    }
+}
---
+++ EloadasProject/Tests/EloadasTest.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EloadasProject.Tests
+{
+    [TestFixture]
+    public class EloadasTest
+    {
+        private Eloadas eloadas;
+
+        [SetUp]
+        public void TestInit()
+        {
+            eloadas = new Eloadas(5, 5);
+        }
+
+        [Test]
+        public void KonstruktorTest1()
+        {
+            Assert.Throws<ArgumentException>(() => new Eloadas(0, 5));
+        }
+
+        [Test]
+        public void KonstruktorTest2()
+        {
+            Assert.Throws<ArgumentException>(() => new Eloadas(5, 0));
+        }
+
+        [Test]
+        public void KonstruktorTest3()
+        {
+            Assert.Throws<ArgumentException>(() => new Eloadas(0, 0));
+        }
+
+        [Test]
+        public void LefoglalTest1()
+        {
+            Assert.AreEqual(true, eloadas.Lefoglal());
+        }
+
+        [Test]
+        public void LefoglalTest2()
+        {
+            Megtoltes();
+            Assert.IsFalse(eloadas.Lefoglal());
+        }
+
+        [Test]
+        public void SzabadHelyekTest1()
+        {
+            Assert.AreEqual(25, eloadas.HelySzabad());
+        }  
+
+        [Test]
+        public void SzabadHelyekEsLefoglalTest()
+        {
+            Megtoltes();
+            Assert.IsFalse(eloadas.Lefoglal());
+            Assert.Zero(eloadas.HelySzabad());
+        }
+
+
+        [Test]
+        public void TeliTest1()
+        {
+            Megtoltes();
+            Assert.IsTrue(eloadas.Full());
+        }
+
+        [Test]
+        public void TeliTest2()
+        {
+            Assert.IsFalse(eloadas.Full());
+        }
+
+        [Test]
+        public void FoglaltTest1()
+        {
+            Megtoltes();
+            Assert.IsTrue(eloadas.Lefoglalva(1, 1));
+        }
+
+        [Test]
+        public void FoglaltTest2()
+        {
+            Assert.Throws<ArgumentException>(() => eloadas.Lefoglalva(0, 1));
+        }
+
+        [Test]
+        public void FoglaltTest3()
+        {
+            Assert.Throws<ArgumentException>(() => eloadas.Lefoglalva(1, 0));
+        }
+
+        [Test]
+        public void FoglaltTest4()
+        {
+            Assert.Throws<ArgumentException>(() => eloadas.Lefoglalva(0, 0));
+        }
+
+        [Test]
+        public void FoglaltTest5()
+        {
+            Assert.IsFalse(eloadas.Lefoglalva(3, 3));
+        }
+
+        private void Megtoltes()
+        {
+            while (!eloadas.Full())
+            {
+                eloadas.Lefoglal();
+            }
+        }
+    }
+}
